004aede3effe6d7faceb7a81300133249b2fa424
codytheroux96 <cody.theroux3@gmail.com>
2024-10-24 20:59:19 -0400

    counting number of words in passage

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     text = get_book_path(book_path)
     print(text)
 
+    number_of_words = word_count(text)
+    print(number_of_words)
+
 def get_book_path(path):
     with open(path) as f:
         return f.read()
+
+def word_count(passage):
+    words = passage.split()
+    return len(words)
 
 main()
